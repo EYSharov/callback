24fa3b77b0bd7f880b01a0c894698a358adb9804
Evgeniy Sharov <99725155+EYSharov@users.noreply.github.com>
2022-02-22 17:21:12 +0500

    Add files via upload

---
+++ action_callback_form.php
@@ -0,0 +1,61 @@
+﻿<?php
+ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tel'])) {
+  $name = strip_tags($_POST['name']); 
+  $tel = strip_tags($_POST['tel']); /*Преобразование в массив без tel, email и name */
+  $email = strip_tags($_POST['email']);
+ }
+ if (empty($name)) {
+ $result = array('name' => "!",    	
+		         'answer' => "Пожалуйста введите свое имя!" ); 
+    echo json_encode($result); 
+	exit;
+}
+
+if (empty($email)) {
+$result = array('name' => $name."!",    	
+            'answer' => "Пожалуйста введите свой email!" ); 
+   echo json_encode($result); 
+ exit;
+}
+
+else {
+  if (stripos ($email , "@")===false )
+  {
+    $result = array('name' => $name."!" , 
+    'answer' => "Пожалуйста введите свой email!" );	
+    echo json_encode($result); 
+   exit;
+  }
+}
+
+ if (empty($tel)) {
+ $result = array('name' => $name."!" , 
+                 'answer' => "Пожалуйста введите свой телефон!" );	
+    echo json_encode($result); 
+	exit;
+}
+ 
+/*Send*/
+   
+elseif (!empty($tel))
+{
+$to = "baa@1cab.ru"; 
+$headers = "Content-type: text/plain; charset = utf-8";
+$headers .= 'From: webmaster@example.com' . "\r\n" .
+$subject = "Заказ обратного звонка на сайте \n";
+$message = "Посетитель $name заказал обратный звонок! \n Телефон: $tel \n Почта: $email ";
+$send = mail ($to, $subject, $message, $headers);
+}
+if ($send=='true'){
+$result = array(	
+    	'name' => $name."!",    	
+		'answer' => "Ваш телефон .$tel и адрес электронной почты $email </br> Мы перезвоним Вам в течении 15 минут!"
+    ); 
+    echo json_encode($result); /*Отправляем массив в аякс */
+	exit;
+}	
+	
+?>
+
+
+
---
+++ ajax_callback.js
@@ -0,0 +1,24 @@
+﻿$( document ).ready(function() {
+    $("#submit_tel").click(
+		function(){
+			sendCallbackForm('result_form', 'callback_form', 'action_callback_form.php');
+			return false; 
+		}
+	);
+}); 
+function sendCallbackForm(result_form, callback_form, url) {
+    $.ajax({
+        url:      "action_callback_form.php", 
+        type:     "POST", //Method sends
+        dataType: "html", //Type send
+        data: $("#"+callback_form).serialize(),  // Serialize  (преобразуем обьект в одну строку)
+        success: function(response) { //Send succes
+        	result = $.parseJSON(response);   
+        	 $('#result_form').html('Здравствуйте '+result.name+'<br> '+result.answer);     	
+		},
+    	error: function(response) { // Send error
+            $('#result_form').html('Ошибка. Данные не отправленны.');
+    	}
+ 	});
+}
+ 
